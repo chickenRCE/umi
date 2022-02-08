1eb00d408ceae29583fdae5720b5271f01f9df3b
Daniel Lim Wee Soong <weesoong.lim@gmail.com>
2022-02-08 15:37:15 +0800

    add flags to challs

--- _nuxt/static/1643899645/omicron/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643899645",layout:"blank",error:a,serverRendered:true,routePath:"\u002Fomicron",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"04fc59a4"}}}}(null));
