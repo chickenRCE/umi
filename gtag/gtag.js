29ddbf4739d132d55bf88e4a947996a827ec1431
chickenRCE <>
2022-02-12 14:49:55 +0000

    Update from https://github.com/chickenRCE/omu/commit/64743034508e3289435d20d9e45179fe9d8b5f4e

---
+++ gtag/gtag.js
@@ -0,0 +1,7 @@
+if (new URL(window.location.href).hostname == "omu.rce.so") {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-NGW2FJW072');
+}
