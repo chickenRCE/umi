a8191fb91fb4d7af44899ad3047f0c829d42bd95
chickenRCE <>
2022-04-04 13:18:43 +0000

    Update from https://github.com/chickenRCE/omu/commit/58293111a7c95b44248ae1dd50e91f8fb56d6bba

--- _nuxt/static/1644677383/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644677383",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"41384735"}}}}(null,"\u002F"));
